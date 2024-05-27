9dd9e186af4e2a6ab698d9e703ab0611fa01df46
Fabricio <fabripicado@gmail.com>
2024-05-26 20:01:49 -0600

    primero

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Proyecto3BD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        
+        <asp:GridView ID="gvdInicio" runat="server" AutoGenerateColumns="true">
+
+        </asp:GridView>
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using System.Data.SqlTypes;
+using System.Web.Services.Description;
+using System.Security.Cryptography;
+
+namespace Proyecto3BD
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+
+
+            }
+        }
+    }
+}
