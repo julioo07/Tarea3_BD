c33df2d05ec5f0f0c18d9360941e9b01f73aed64
BERMUDEZ JIMENEZ JULIO <juliober07@estudiantec.cr>
2024-06-10 19:37:49 -0600

    Muestra las facturas

--- Default.aspx
+++ Default.aspx
@@ -4,19 +4,97 @@
 
     <main>
         
-        <asp:GridView ID="gvdInicio" runat="server" AutoGenerateColumns="true">
+        <asp:Panel ID="pnlMenuPrincipal" runat="server" Visible="true">
+            <div> 
 
-        </asp:GridView>
+                <br/>
+                SISTEMA DE FACTURACIÓN DE SERVICIOS TELEFÓNICOS
+                <br />
+                <br />
+                <!-- Sección de entrada de texto para buscar -->
+                <asp:TextBox ID="TextBoxSearch" runat="server" placeholder="Número telefónico"></asp:TextBox>
+                <!-- Botón para buscar -->
+                <asp:Button ID="btnSearch" runat="server" Text="Buscar" OnClick="btnSearch_Click" />
+                <br />
+                <br />
+                <!-- Lista de los empleados + boton que sale junto a cada uno -->
+                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" OnRowCommand="GridView1_RowCommand">
+                    <Columns>
+                        <asp:BoundField DataField="Id" HeaderText="ID Factura" />
+                        <asp:BoundField DataField="Total" HeaderText="Total" />
+                        <asp:BoundField DataField="TotalMasIVA" HeaderText="+ I.V.A." />
+                        <asp:BoundField DataField="Estado" HeaderText="Estado" />
+                        <asp:ButtonField ButtonType="Button" Text="Ver Detalle" CommandName="Accion" />
+
+     
+                    </Columns>
+                </asp:GridView>
+
+                <br />
+                <br />
+                
+                ESTADOS DE CUENTA
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="Empresa X" OnClick="btnSearch_Click" />
+                <asp:Button ID="Button2" runat="server" Text="Empresa Y" OnClick="btnSearch_Click" />
+
+            <div> 
+        </asp:Panel>
+
+    
 
 
+        <asp:Panel ID="pnlSeleccionEmpleado" runat="server" Visible="false">
 
+            Factura seleccionado: 
+            <asp:Label ID="lblIdFactura" runat="server"></asp:Label> <br />
+            <br />
+            <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:TemplateField HeaderText="Desglose de Factura">
+                        <ItemTemplate>
+                            <table>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Fecha de Cierre:</td>
+                                    <td><%# Eval("FechaCierre") %></td>
+                                </tr>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Total:</td>
+                                    <td><%# Eval("Total") %></td>
+                                </tr>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Total + I.V.A.:</td>
+                                    <td><%# Eval("TotalMasIVA") %></td>
+                                </tr>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Multa:</td>
+                                    <td><%# Eval("Multa") %></td>
+                                </tr>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Total + Multa:</td>
+                                    <td><%# Eval("TotalMasMulta") %></td>
+                                </tr>
+                                <tr class="grid-row-spacing"><td colspan="2"></td></tr>
+                                <tr>
+                                    <td>Fecha de Pago:</td>
+                                    <td><%# Eval("FechaPago") %></td>
+                                </tr>
+                            </table>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <br />
+            <!-- Botón para regresar -->
+            <asp:Button ID="BtnRegresarSelec" runat="server" Text="Regresar" OnClick="btnRegresar_Click" />
 
-
-
-
-
-
-
+        </asp:Panel>
 
 
     </main>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -17,13 +17,119 @@ namespace Proyecto3BD
 {
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+         protected void Page_Load(object sender, EventArgs e)
         {
 
             if (!IsPostBack)
             {
 
 
+            }
+        }
+
+        protected void btnRegresar_Click(Object sender, EventArgs e)
+        {
+            pnlMenuPrincipal.Visible = true;
+            pnlSeleccionEmpleado.Visible = false;
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPSFiltrarFacturas"; // Utiliza el procedimiento almacenado de filtro
+                cmd.Connection = conn;
+
+                cmd.Parameters.AddWithValue("@Entrada", TextBoxSearch.Text.Trim());
+
+                // Agregar parámetro de salida
+                SqlParameter outParameter = new SqlParameter("@OutResulTCode", SqlDbType.Int);
+                outParameter.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(outParameter);
+
+                conn.Open();
+
+                // Ejecuta el SP y obtiene los resultados en un SqlDataReader
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                    if (reader.HasRows)
+                    {
+                        // Si hay filas, enlaza el resultado al GridView
+                        GridView1.DataSource = reader;
+                        GridView1.DataBind();
+                    }
+                    else
+                    {
+                        // Si ningun empleado coincide con el filtro que se puso
+                        Response.Write("No se encontraron resultados.");
+                    }
+                }
+
+                // Después de ejecutar el comando, puedes obtener el valor del parámetro de salida
+                int resultado = (int)cmd.Parameters["@OutResulTCode"].Value;
+                // Hacer algo con el valor de resultado si es necesario
+                if (resultado == 1)
+                {
+                   
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Accion")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+
+                pnlMenuPrincipal.Visible = false;
+                pnlSeleccionEmpleado.Visible = true;
+
+                GridViewRow selectedRow = GridView1.Rows[index];
+                
+                string IdPuesto = selectedRow.Cells[0].Text;
+                lblIdFactura.Text = IdPuesto;
+
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "SPSFactura"; // Utiliza el procedimiento almacenado de filtro
+                    cmd.Connection = conn;
+
+                    cmd.Parameters.AddWithValue("@Entrada", IdPuesto);
+
+                    // Agregar parámetro de salida
+                    SqlParameter outParameter = new SqlParameter("@OutResulTCode", SqlDbType.Int);
+                    outParameter.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(outParameter);
+
+                    conn.Open();
+
+                    // Ejecuta el SP y obtiene los resultados en un SqlDataReader
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        if (reader.HasRows)
+                        {
+                            // Si hay filas, enlaza el resultado al GridView
+                            GridView2.DataSource = reader;
+                            GridView2.DataBind();
+                        }
+                        else
+                        {
+                            // Si ningun empleado coincide con el filtro que se puso
+                            Response.Write("No se encontraron resultados.");
+                        }
+                    }
+                }
             }
         }
     }
